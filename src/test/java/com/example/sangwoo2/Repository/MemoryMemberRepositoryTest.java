eb9afe54747d0e1214bd7a9451832a0ebe9984ff
SWYeo118 <effortmeta@gmail.com>
2022-12-06 11:04:02 +0900

    spring Test 부분 작성하다가 커밋

--- src/test/java/com/example/sangwoo2/Repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/example/sangwoo2/Repository/MemoryMemberRepositoryTest.java
@@ -1,9 +1,11 @@
 package com.example.sangwoo2.Repository;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.example.sangwoo2.Domain.Member;
+
+import java.util.Optional;
 
 class MemoryMemberRepositoryTest {
   
@@ -15,6 +17,19 @@ class MemoryMemberRepositoryTest {
     member.setName("spring");
     repository.save(member);
     Member result = repository.findById(member.getId()).get();
-    Assertions.assertEquals(member, result);
+    Assertions.assertThat(member).isEqualTo(result);
+  }
+
+  @Test
+  public void findByName() {
+    Member member1 = new Member();
+    member1.setName("spring1");
+    repository.save(member1);
+
+    Member member2 = new Member();
+    member2.setName("spring2");
+    repository.save(member2);
+
+    Member result = repository.findByName("spring1").get();
   }
 }
